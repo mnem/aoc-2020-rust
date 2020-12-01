4deb6f99e775a303fa2517eb57f8213978195c0a
David Wagner <mnem@noiseandheat.com>
2020-12-01 19:52:19 +0000

    Day 1b

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,6 +3,9 @@ use common::Puzzle;
 fn main() {
     let mut a: Puzzle1 = Default::default();
     a.run();
+
+    let mut b: Puzzle2 = Default::default();
+    b.run();
 }
 
 #[derive(Default)]
@@ -24,6 +27,35 @@ impl Puzzle for Puzzle1 {
                 let entry_b = self.values[j];
                 if entry_a + entry_b == 2020 {
                     return (entry_a * entry_b).to_string();
+                }
+            }
+        }
+        return "Not found".to_string();
+    }
+}
+
+#[derive(Default)]
+struct Puzzle2 {
+    values: Vec<i64>
+}
+
+impl Puzzle for Puzzle2 {
+    type ParsedLine = i64;
+
+    fn process_item(&mut self, item: Self::ParsedLine) {
+        self.values.push(item);
+    }
+
+    fn final_result(&mut self) -> String {
+        for i in 0..(self.values.len() - 2) {
+            let entry_a = self.values[i];
+            for j in (i + 1)..(self.values.len() - 1) {
+                let entry_b = self.values[j];
+                for k in (j + 1)..self.values.len() {
+                    let entry_c = self.values[k];
+                    if entry_a + entry_b + entry_c == 2020 {
+                        return (entry_a * entry_b * entry_c).to_string();
+                    }
                 }
             }
         }
