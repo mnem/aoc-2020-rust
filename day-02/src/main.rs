11d4d8ff4e1b4c13a7b7c18b2ee1c8622dd1408e
David Wagner <mnem@noiseandheat.com>
2020-12-02 16:39:44 +0000

    Day 2, b

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     let mut a: Puzzle1 = Default::default();
     a.run();
 
-    // let mut b: Puzzle2 = Default::default();
-    // b.run();
+    let mut b: Puzzle2 = Default::default();
+    b.run();
 }
 
 #[derive(Default)]
@@ -15,20 +15,25 @@ struct Puzzle1 {
     invalid_passwords: i64,
 }
 
-impl Puzzle for Puzzle1 {
-    type ParsedLine = String;
-
-    fn process_item(&mut self, item: Self::ParsedLine) {
-        //2-9 c: ccccccccc
+impl Puzzle1 {
+    fn is_valid(rule: String) -> bool {
         let min: usize;
         let max: usize;
         let letter: String;
         let password: String;
-        let mut chars = item.into_bytes().into_iter();
+        let mut chars = rule.into_bytes().into_iter();
         scan!(chars => "{}-{} {}: {}", min, max, letter, password);
 
         let occurrences = password.matches(&letter).count();
-        if (min..=max).contains(&occurrences) {
+        (min..=max).contains(&occurrences)
+    }
+}
+
+impl Puzzle for Puzzle1 {
+    type ParsedLine = String;
+
+    fn process_item(&mut self, item: Self::ParsedLine) {
+        if Puzzle1::is_valid(item) {
             self.valid_passwords += 1;
         } else {
             self.invalid_passwords += 1;
@@ -40,31 +45,63 @@ impl Puzzle for Puzzle1 {
     }
 }
 
-// #[derive(Default)]
-// struct Puzzle2 {
-//     values: Vec<i64>,
-// }
-//
-// impl Puzzle for Puzzle2 {
-//     type ParsedLine = i64;
-//
-//     fn process_item(&mut self, item: Self::ParsedLine) {
-//         self.values.push(item);
-//     }
-//
-//     fn final_result(&mut self) -> String {
-//         for i in 0..(self.values.len() - 2) {
-//             let entry_a = self.values[i];
-//             for j in (i + 1)..(self.values.len() - 1) {
-//                 let entry_b = self.values[j];
-//                 for k in (j + 1)..self.values.len() {
-//                     let entry_c = self.values[k];
-//                     if entry_a + entry_b + entry_c == 2020 {
-//                         return (entry_a * entry_b * entry_c).to_string();
-//                     }
-//                 }
-//             }
-//         }
-//         "Not found".to_string()
-//     }
-// }
+#[derive(Default)]
+struct Puzzle2 {
+    valid_passwords: i64,
+    invalid_passwords: i64,
+}
+
+impl Puzzle2 {
+    fn is_valid(rule: String) -> bool {
+        let pos_a: usize;
+        let pos_b: usize;
+        let letter: char;
+        let password: String;
+        let mut chars = rule.into_bytes().into_iter();
+        scan!(chars => "{}-{} {}: {}", pos_a, pos_b, letter, password);
+
+
+        let pos_a_char = password.chars().nth(pos_a - 1).expect("Pos A OOB");
+        let pos_b_char = password.chars().nth(pos_b - 1).expect("Pos B OOB");
+
+        let in_pos_a = letter == pos_a_char;
+        let in_pos_b = letter == pos_b_char;
+
+        in_pos_a ^ in_pos_b
+    }
+}
+
+impl Puzzle for Puzzle2 {
+    type ParsedLine = String;
+
+    fn process_item(&mut self, item: Self::ParsedLine) {
+        if Puzzle2::is_valid(item) {
+            self.valid_passwords += 1;
+        } else {
+            self.invalid_passwords += 1;
+        }
+    }
+
+    fn final_result(&mut self) -> String {
+        self.valid_passwords.to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(true, Puzzle1::is_valid("1-3 a: abcde".to_string()));
+        assert_eq!(false, Puzzle1::is_valid("1-3 b: cdefg".to_string()));
+        assert_eq!(true, Puzzle1::is_valid("2-9 c: ccccccccc".to_string()));
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(true, Puzzle2::is_valid("1-3 a: abcde".to_string()));
+        assert_eq!(false, Puzzle2::is_valid("1-3 b: cdefg".to_string()));
+        assert_eq!(false, Puzzle2::is_valid("2-9 c: ccccccccc".to_string()));
+    }
+}
