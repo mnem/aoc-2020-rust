a10eee23e17d029c46f9e289581b082d81031ca2
David Wagner <mnem@noiseandheat.com>
2020-12-03 19:25:22 +0000

    Prep for day 4

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -4,9 +4,6 @@ use std::cmp::max;
 fn main() {
     let mut a: Puzzle1 = Default::default();
     a.run();
-
-    // let mut b: Puzzle2 = Default::default();
-    // b.run();
 }
 
 #[derive(PartialEq, Copy, Clone, Debug)]
@@ -73,64 +70,3 @@ impl Puzzle for Puzzle1 {
         format!("{}, {}", trees_b, (trees_a * trees_b * trees_c * trees_d * trees_e))
     }
 }
-
-// #[derive(Default)]
-// struct Puzzle2 {
-//     valid_passwords: i64,
-//     invalid_passwords: i64,
-// }
-//
-// impl Puzzle2 {
-//     fn is_valid(rule: String) -> bool {
-//         let pos_a: usize;
-//         let pos_b: usize;
-//         let letter: char;
-//         let password: String;
-//         let mut chars = rule.into_bytes().into_iter();
-//         scan!(chars => "{}-{} {}: {}", pos_a, pos_b, letter, password);
-//
-//
-//         let pos_a_char = password.chars().nth(pos_a - 1).expect("Pos A OOB");
-//         let pos_b_char = password.chars().nth(pos_b - 1).expect("Pos B OOB");
-//
-//         let in_pos_a = letter == pos_a_char;
-//         let in_pos_b = letter == pos_b_char;
-//
-//         in_pos_a ^ in_pos_b
-//     }
-// }
-
-// impl Puzzle for Puzzle2 {
-//     type ParsedLine = String;
-//
-//     fn process_item(&mut self, item: Self::ParsedLine) {
-//         if Puzzle2::is_valid(item) {
-//             self.valid_passwords += 1;
-//         } else {
-//             self.invalid_passwords += 1;
-//         }
-//     }
-//
-//     fn final_result(&mut self) -> String {
-//         self.valid_passwords.to_string()
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn example_1() {
-//         assert_eq!(true, Puzzle1::is_valid("1-3 a: abcde".to_string()));
-//         assert_eq!(false, Puzzle1::is_valid("1-3 b: cdefg".to_string()));
-//         assert_eq!(true, Puzzle1::is_valid("2-9 c: ccccccccc".to_string()));
-//     }
-//
-//     #[test]
-//     fn example_2() {
-//         assert_eq!(true, Puzzle2::is_valid("1-3 a: abcde".to_string()));
-//         assert_eq!(false, Puzzle2::is_valid("1-3 b: cdefg".to_string()));
-//         assert_eq!(false, Puzzle2::is_valid("2-9 c: ccccccccc".to_string()));
-//     }
-// }
---
+++ day-04/src/main.rs
@@ -0,0 +1,136 @@
+use common::Puzzle;
+use std::cmp::max;
+
+fn main() {
+    let mut a: Puzzle1 = Default::default();
+    a.run();
+
+    // let mut b: Puzzle2 = Default::default();
+    // b.run();
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum GroundState {
+    Open,
+    Tree,
+}
+
+type Line = Vec<GroundState>;
+
+#[derive(Default)]
+struct Puzzle1 {
+    slope: Vec<Line>,
+    max_line: usize
+}
+
+impl Puzzle1 {
+    fn to_line(input: String) -> Line {
+        let mut line = Line::new();
+        for c in input.chars() {
+            let state = match c {
+                '#' => GroundState::Tree,
+                '.' => GroundState::Open,
+                _ => panic!()
+            };
+            line.push(state);
+        }
+
+        line
+    }
+
+    fn count_trees(&self, x_step: usize, y_step: usize) -> i64 {
+        let mut trees = 0;
+        let mut x = x_step;
+        let mut y = y_step;
+        while y < self.slope.len() {
+            if self.slope[y][x % self.max_line] == GroundState::Tree {
+                trees += 1;
+            }
+            x += x_step;
+            y += y_step;
+        }
+
+        trees
+    }
+}
+
+impl Puzzle for Puzzle1 {
+    type ParsedLine = String;
+
+    fn process_item(&mut self, item: Self::ParsedLine) {
+        let line = Puzzle1::to_line(item);
+        self.max_line = max(self.max_line, line.len());
+        self.slope.push(line);
+    }
+
+    fn final_result(&mut self) -> String {
+        let trees_a = self.count_trees(1, 1);
+        let trees_b = self.count_trees(3, 1);
+        let trees_c = self.count_trees(5, 1);
+        let trees_d = self.count_trees(7, 1);
+        let trees_e = self.count_trees(1, 2);
+
+        format!("{}, {}", trees_b, (trees_a * trees_b * trees_c * trees_d * trees_e))
+    }
+}
+
+// #[derive(Default)]
+// struct Puzzle2 {
+//     valid_passwords: i64,
+//     invalid_passwords: i64,
+// }
+//
+// impl Puzzle2 {
+//     fn is_valid(rule: String) -> bool {
+//         let pos_a: usize;
+//         let pos_b: usize;
+//         let letter: char;
+//         let password: String;
+//         let mut chars = rule.into_bytes().into_iter();
+//         scan!(chars => "{}-{} {}: {}", pos_a, pos_b, letter, password);
+//
+//
+//         let pos_a_char = password.chars().nth(pos_a - 1).expect("Pos A OOB");
+//         let pos_b_char = password.chars().nth(pos_b - 1).expect("Pos B OOB");
+//
+//         let in_pos_a = letter == pos_a_char;
+//         let in_pos_b = letter == pos_b_char;
+//
+//         in_pos_a ^ in_pos_b
+//     }
+// }
+
+// impl Puzzle for Puzzle2 {
+//     type ParsedLine = String;
+//
+//     fn process_item(&mut self, item: Self::ParsedLine) {
+//         if Puzzle2::is_valid(item) {
+//             self.valid_passwords += 1;
+//         } else {
+//             self.invalid_passwords += 1;
+//         }
+//     }
+//
+//     fn final_result(&mut self) -> String {
+//         self.valid_passwords.to_string()
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn example_1() {
+//         assert_eq!(true, Puzzle1::is_valid("1-3 a: abcde".to_string()));
+//         assert_eq!(false, Puzzle1::is_valid("1-3 b: cdefg".to_string()));
+//         assert_eq!(true, Puzzle1::is_valid("2-9 c: ccccccccc".to_string()));
+//     }
+//
+//     #[test]
+//     fn example_2() {
+//         assert_eq!(true, Puzzle2::is_valid("1-3 a: abcde".to_string()));
+//         assert_eq!(false, Puzzle2::is_valid("1-3 b: cdefg".to_string()));
+//         assert_eq!(false, Puzzle2::is_valid("2-9 c: ccccccccc".to_string()));
+//     }
+// }
