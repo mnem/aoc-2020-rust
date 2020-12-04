240ac66835758cca68f5b04f94ad5f3a05aa6e62
David Wagner <mnem@noiseandheat.com>
2020-12-04 23:09:17 +0000

    Keep pressing compile until it works.

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -4,11 +4,11 @@ use std::collections::HashSet;
 use regex::Regex;
 
 fn main() {
-    let mut a = Puzzle1::new();
+    let mut a = Puzzle1::new(RuleSet::Casual);
     a.run();
 
-    // let mut b: Puzzle2 = Default::default();
-    // b.run();
+    let mut b = Puzzle1::new(RuleSet::Strict);
+    b.run();
 }
 
 #[derive(Hash, Clone, Copy, Eq, PartialEq, Debug)]
@@ -19,6 +19,12 @@ enum PassportFields {
 struct Rule {
     field: PassportFields,
     matcher: Regex,
+    match_validation: Option<Box<dyn Fn(String) -> bool>>
+}
+
+enum RuleSet {
+    Strict,
+    Casual,
 }
 
 #[derive(Default)]
@@ -30,37 +36,11 @@ struct Puzzle1 {
 }
 
 impl Puzzle1 {
-    fn new() -> Puzzle1 {
-        let mut rules = Vec::new();
-
-        rules.push( Rule {
-            field: PassportFields::Byr,
-            matcher: Regex::new(r"byr:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Iyr,
-            matcher: Regex::new(r"iyr:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Eyr,
-            matcher: Regex::new(r"eyr:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Hgt,
-            matcher: Regex::new(r"hgt:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Hcl,
-            matcher: Regex::new(r"hcl:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Ecl,
-            matcher: Regex::new(r"ecl:(.+?)\b").expect("Whoops")
-        } );
-        rules.push( Rule {
-            field: PassportFields::Pid,
-            matcher: Regex::new(r"pid:(.+?)\b").expect("Whoops")
-        } );
+    fn new(strict: RuleSet) -> Puzzle1 {
+        let rules = match strict {
+            RuleSet::Strict => Puzzle1::make_strict_rules(),
+            RuleSet::Casual => Puzzle1::make_casual_rules(),
+        };
 
         Puzzle1 { building: Default::default(), valid_count: Default::default(), required_fields: rules }
     }
@@ -82,6 +62,102 @@ impl Puzzle1 {
             self.valid_count += 1;
         }
     }
+
+    fn make_strict_rules() -> Vec<Rule> {
+        let mut rules = Vec::new();
+        rules.push( Rule {
+            field: PassportFields::Byr,
+            matcher: Regex::new(r"byr:(\d{4})\b").expect("Whoops"),
+            match_validation: Some(Box::new(|s: String| -> bool {
+                (1920..=2002).contains(&s.parse().unwrap())
+            })),
+        } );
+        rules.push( Rule {
+            field: PassportFields::Iyr,
+            matcher: Regex::new(r"iyr:(\d{4})\b").expect("Whoops"),
+            match_validation: Some(Box::new(|s: String| -> bool {
+                (2010..=2020).contains(&s.parse().unwrap())
+            })),
+        } );
+        rules.push( Rule {
+            field: PassportFields::Eyr,
+            matcher: Regex::new(r"eyr:(\d{4})\b").expect("Whoops"),
+            match_validation: Some(Box::new(|s: String| -> bool {
+                (2020..=2030).contains(&s.parse().unwrap())
+            })),
+        } );
+        rules.push( Rule {
+            field: PassportFields::Hgt,
+            matcher: Regex::new(r"hgt:(\d+?(cm|in))\b").expect("Whoops"),
+            match_validation: Some(Box::new(|s: String| -> bool {
+                let c = Regex::new(r"(\d+)(.+)").unwrap().captures(&s).unwrap();
+                let n: i32 = c.get(1).unwrap().as_str().to_string().parse().unwrap();
+                match c.get(2).unwrap().as_str() {
+                    "cm" => (150..=193).contains(&n),
+                    "in" => (59..=76).contains(&n),
+                    _ => panic!()
+                }
+            })),
+        } );
+        rules.push( Rule {
+            field: PassportFields::Hcl,
+            matcher: Regex::new(r"hcl:#([0-9a-f]{6})\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Ecl,
+            matcher: Regex::new(r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Pid,
+            matcher: Regex::new(r"pid:(\d{9})\b").expect("Whoops"),
+            match_validation: None,
+        } );
+
+        rules
+    }
+
+    fn make_casual_rules() -> Vec<Rule> {
+        let mut rules = Vec::new();
+        rules.push( Rule {
+            field: PassportFields::Byr,
+            matcher: Regex::new(r"byr:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Iyr,
+            matcher: Regex::new(r"iyr:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Eyr,
+            matcher: Regex::new(r"eyr:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Hgt,
+            matcher: Regex::new(r"hgt:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Hcl,
+            matcher: Regex::new(r"hcl:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Ecl,
+            matcher: Regex::new(r"ecl:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+        rules.push( Rule {
+            field: PassportFields::Pid,
+            matcher: Regex::new(r"pid:(.+?)\b").expect("Whoops"),
+            match_validation: None,
+        } );
+
+        rules
+    }
 }
 
 impl Puzzle for Puzzle1 {
@@ -98,8 +174,17 @@ impl Puzzle for Puzzle1 {
 
     fn process_item(&mut self, item: Self::ParsedLine) {
         for rule in &self.required_fields {
-            if rule.matcher.captures(&item).is_some() {
-                self.building.insert(rule.field);
+            if let Some(c) = rule.matcher.captures(&item) {
+                match &rule.match_validation {
+                    None => {
+                        self.building.insert(rule.field);
+                    },
+                    Some(v) => {
+                        if v(c.get(1).unwrap().as_str().to_string()) {
+                            self.building.insert(rule.field);
+                        }
+                    }
+                };
             }
         }
     }
@@ -110,48 +195,6 @@ impl Puzzle for Puzzle1 {
     }
 }
 
-// #[derive(Default)]
-// struct Puzzle2 {
-//     valid_passwords: i64,
-//     invalid_passwords: i64,
-// }
-//
-// impl Puzzle2 {
-//     fn is_valid(rule: String) -> bool {
-//         let pos_a: usize;
-//         let pos_b: usize;
-//         let letter: char;
-//         let password: String;
-//         let mut chars = rule.into_bytes().into_iter();
-//         scan!(chars => "{}-{} {}: {}", pos_a, pos_b, letter, password);
-//
-//
-//         let pos_a_char = password.chars().nth(pos_a - 1).expect("Pos A OOB");
-//         let pos_b_char = password.chars().nth(pos_b - 1).expect("Pos B OOB");
-//
-//         let in_pos_a = letter == pos_a_char;
-//         let in_pos_b = letter == pos_b_char;
-//
-//         in_pos_a ^ in_pos_b
-//     }
-// }
-
-// impl Puzzle for Puzzle2 {
-//     type ParsedLine = String;
-//
-//     fn process_item(&mut self, item: Self::ParsedLine) {
-//         if Puzzle2::is_valid(item) {
-//             self.valid_passwords += 1;
-//         } else {
-//             self.invalid_passwords += 1;
-//         }
-//     }
-//
-//     fn final_result(&mut self) -> String {
-//         self.valid_passwords.to_string()
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -159,15 +202,8 @@ mod tests {
     #[test]
     fn example_1() {
         let input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\nhcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm\n\nhcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in\n".to_string();
-        let mut subject: Puzzle1 = Puzzle1::new();
+        let mut subject: Puzzle1 = Puzzle1::new(RuleSet::Casual);
         subject.run_with_input(input);
         assert_eq!(2, subject.valid_count);
     }
-
-    // #[test]
-    // fn example_2() {
-    //     assert_eq!(true, Puzzle2::is_valid("1-3 a: abcde".to_string()));
-    //     assert_eq!(false, Puzzle2::is_valid("1-3 b: cdefg".to_string()));
-    //     assert_eq!(false, Puzzle2::is_valid("2-9 c: ccccccccc".to_string()));
-    // }
 }
